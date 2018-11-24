737c94ba3d005ee04ff897b941d72d31ff20a2a3
Andrea <andybbruno@gmail.com>
2018-11-24 15:26:42 +0100

    Runs now works
    Co-Authored-By: michelecafagna26 <mikele.c@outlook.it>

--- views/__init__.py
+++ views/__init__.py
@@ -3,11 +3,11 @@ from .auth import auth
 from .users import users
 # from .strava import strava
 from .comparisons import comparisons
-# from .runs import runs
+from .runs import runs
 from .objective import objective
 from .challenge import challenge
 
 #blueprints = [home, auth, users, strava, comparisons, runs, objective, challenge]
-blueprints = [users, home, auth, objective, comparisons, challenge]
+blueprints = [users, runs, home, auth, objective, comparisons, challenge]
 
 
--- views/runs.py
+++ views/runs.py
@@ -13,18 +13,21 @@ runs = Blueprint('runs', __name__)
 def _runs(run_id):
 
     run_list = []
+    run = None
+    prev_run = None
     if current_user is not None and hasattr(current_user, 'id'):
         #runs = db.session.query(Run).filter(Run.runner_id == current_user.id, Run.id == run_id).first()
         #prev_run = db.session.query(Run).filter(Run.runner_id == current_user.id, Run.id < run_id).order_by(Run.id.desc()).first()
         runs_json = requests.get(DATASERVICE + '/users/' + str(current_user.id) + '/runs').json()
+    
         for run_json in runs_json:
             run_list.append(Run(run_json))
+            if run_list[-1].id == run_id:
+                run = run_list[-1]
+                
+                if len(run_list) == 1:
+                    prev_run = run
+                else:
+                    prev_run = run_list[-2]
 
-        runs = runs_list[run_id]
-        if run_id > 0
-            prev_run = runs_list[run_id -1]
-
-    else:
-        runs = None
-        prev_run = None;
-    return render_template("runs.html", run=runs, prev_run=prev_run)
+    return render_template("runs.html", run=run, prev_run=prev_run)
