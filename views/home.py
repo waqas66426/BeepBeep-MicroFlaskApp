b9ad37d69b75ff2d2b22bd81fc42e0b095b7c833
Andrea <andybbruno@gmail.com>
2018-11-24 11:08:12 +0100

    Challenge works + in "home.py" added pace-based comparsion

--- views/home.py
+++ views/home.py
@@ -12,10 +12,12 @@ from auth import current_user
 from forms import ObjectiveForm
 from views.auth import *
 from views.util import *
-import requests, json
+import requests
+import json
 from config import DATASERVICE
 
 home = Blueprint('home', __name__)
+
 
 def _strava_auth_url(config):
     client = Client()
@@ -25,13 +27,14 @@ def _strava_auth_url(config):
                                    redirect_uri=redirect)
     return url
 
+
 @home.route('/')
 def index():
     if request.args.get('comparisonError') is None:
         comparisonError = ""
     else:
         comparisonError = request.args.get('comparisonError')
-    
+
     if request.args.get('challengeError') is None:
         challengeError = ""
     else:
@@ -46,8 +49,9 @@ def index():
     sec = 0
 
     if current_user is not None and hasattr(current_user, 'id'):
-        runListDict = requests.get(DATASERVICE + '/users/' + str(current_user.id) + '/runs').json()
-        
+        runListDict = requests.get(
+            DATASERVICE + '/users/' + str(current_user.id) + '/runs').json()
+
         runs = []
         for r in runListDict:
             runs.append(Run(r))
@@ -61,35 +65,54 @@ def index():
 
             minutes, sec = sec2minsec(elapsed_time)
 
-        #objective = None#db.session.query(Objectives).filter(Objectives.user_id == current_user.id).first()
-        objective = requests.get(DATASERVICE + '/users/' + str(current_user.id) + '/objectives').json()
+        # objective = None#db.session.query(Objectives).filter(Objectives.user_id == current_user.id).first()
+        objective = requests.get(
+            DATASERVICE + '/users/' + str(current_user.id) + '/objectives').json()
         if objective:
             objective_distance = objective[0]['distance']
-        
-        #handling challenges
-        #colored lists for runs to be challenged
+
+        # handling challenges
+        # colored lists for runs to be challenged
         yellow = []
         red = []
         green = []
         orange = []
-        #fetch the run selected for the challenge
-        challenged_run_reply = requests.get(DATASERVICE + '/users/' + str(current_user.id) + '/challenges').json()
+        # fetch the run selected for the challenge
+        challenged_run_reply = requests.get(
+            DATASERVICE + '/users/' + str(current_user.id) + '/challenges').json()
         if challenged_run_reply:
             challenged_run = challenged_run_reply[0]
-            #the challenged run is print in yellow
+            # the challenged run is print in yellow
             yellow.append(challenged_run['run_id'])
-            #fetching runs stored only after the selection of the challenged run
-            after_challenge_run = []
+            # fetching runs stored only after the selection of the challenged run
+            #after_challenge_run = []
             #db.session.query(Run).filter(current_user.id == Run.runner_id, Run.id > challenged_run.latest_run_id).all()
-            #fills appropriate lists depending on run performances
-            for run in after_challenge_run:                
-                if run.average_speed > challenged_run.run.average_speed and run.distance > challenged_run.run.distance:
-                    green.append(run.id)
-                elif run.average_speed <= challenged_run.run.average_speed and run.distance <= challenged_run.run.distance:
-                    red.append(run.id)
-                else: 
-                    orange.append(run.id)
-    
+
+            ch_run = last_run = requests.get(
+                DATASERVICE + '/users/' + str(current_user.id) + '/runs/' + str(challenged_run['run_id'])).json()
+
+            last_run_id = requests.get(
+                DATASERVICE + '/users/' + str(current_user.id) + '/runs/getMaxId').json()['max_id']
+
+            last_run = requests.get(
+                DATASERVICE + '/users/' + str(current_user.id) + '/runs/' + str(last_run_id)).json()
+            print("@@@@@@@@@@@@@@@@@@@@@")
+            print(challenged_run)
+            print(ch_run)
+            print(last_run)
+            print("@@@@@@@@@@@@@@@@@@@@@")
+
+            last_run_pace = last_run['distance'] / last_run['elapsed_time']
+            ch_run_pace = ch_run['distance'] / ch_run['elapsed_time']
+
+            # fills appropriate lists depending on run performances
+            if last_run_pace > ch_run_pace:
+                green.append(last_run['id'])
+            elif last_run_pace <= ch_run_pace:
+                red.append(last_run['id'])
+            else:
+                orange.append(last_run['id'])
+
     else:
         return redirect("/login")
 
@@ -103,21 +126,19 @@ def index():
 
     return render_template(
         "index.html",
-        runs = runs,
-        strava_auth_url = strava_auth_url,
-        avgSpeed = mh2kmh(avgSpeed),
-        minutes = minutes,
-        sec = sec,
-        comparisonError = comparisonError,
-        challengeError = challengeError,
-        objective_distance = m2km(objective_distance),
-        tot_distance = m2km(tot_distance),
-        remaining_KM = m2km(remaining_KM),
-        percentage = percentage,
-        challenge_orange = orange,
-        challenge_red = red,
-        challenge_green = green,
-        challenged_run = yellow
+        runs=runs,
+        strava_auth_url=strava_auth_url,
+        avgSpeed=mh2kmh(avgSpeed),
+        minutes=minutes,
+        sec=sec,
+        comparisonError=comparisonError,
+        challengeError=challengeError,
+        objective_distance=m2km(objective_distance),
+        tot_distance=m2km(tot_distance),
+        remaining_KM=m2km(remaining_KM),
+        percentage=percentage,
+        challenge_orange=orange,
+        challenge_red=red,
+        challenge_green=green,
+        challenged_run=yellow
     )
-        
-
